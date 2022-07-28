a40e63ca96ec814187e3d0e8c9f2b6c6f76b7d8b
ali mohammadinia <alimohammadinia314@yahoo.com>
2022-07-29 01:30:26 +0430

    fastfood-RER-1dev:feat(Products & ProductsTab):create Products & ProductsTab components

---
+++ components/Products/Product/Product.jsx
@@ -0,0 +1,44 @@
+import { numberFormat } from "lib/helper";
+import Image from "next/image";
+
+const Product = ({ product }) => {
+  return (
+    <div className="box">
+      <div>
+        <div className="img-box">
+          <Image
+            className="img-fluid"
+            src={product.primary_image}
+            layout="responsive"
+            width={366}
+            height={244}
+            placeholder="blur"
+            blurDataURL={product.primary_image_blurDataURL}
+          />
+        </div>
+        <div className="detail-box">
+          <h5>{product.name}</h5>
+          <p>{product.description}</p>
+          <div className="options">
+            <h6>
+              {product.is_sale ? (
+                <>
+                  <span>{numberFormat(product.sale_price)}</span>
+                  <del className="me-1">{numberFormat(product.price)}</del>
+                </>
+              ) : (
+                <span>{numberFormat(product.price)}</span>
+              )}
+              <span>تومان</span>
+            </h6>
+            <a href="">
+              <i className="bi bi-cart-fill text-white fs-5"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,42 @@ import About from "@/components/About/About";
 import Features from "@/components/Features/Features";
 import ContactUs from "@/components/Contact/ContactUs/ContactUs";
 
+import axios from "axios";
+import { handleError } from "lib/helper";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
+import ProductsTab from "@/components/Products/ProductsTab/ProductsTab";
 
 
-export default function Home() {
+export default function Home({ productsTab, error }) {
+  useEffect(() => {
+    error && toast.error(error);
+  }, [error]);
+
   return (
     <>
       <Features />
+      {productsTab && <ProductsTab tabs={productsTab} />}
       <About />
       <ContactUs />
-   </>
-  )
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  try {
+    const res = await axios.get("/products/products-tabs");
+    // console.log(res.data.data);
+    return {
+      props: {
+        productsTab: res.data.data,
+      },
+    };
+  } catch (err) {
+    return {
+      props: {
+        error: handleError(err),
+      },
+    };
+  }
 }

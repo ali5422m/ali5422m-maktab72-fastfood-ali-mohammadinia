f74d83c77212029562d4988ba82afb231af2b71d
ali mohammadinia <alimohammadinia314@yahoo.com>
2022-08-10 23:46:20 +0430

    fastfood-RER-1dev:feat(404):create custom page 404

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="d-flex justify-content-center align-items-center layout_padding-137px ">
+      ارور 404 . صفحه مورد نظر یافت نشد
+    </div>
+  );
+};
+
+export default PageNotFound;

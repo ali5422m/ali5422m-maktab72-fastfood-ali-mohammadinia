7589870a03f3a19288c615ad9895201079a5ee3a
ali mohammadinia <alimohammadinia314@yahoo.com>
2022-08-24 08:01:29 +0430

    fastfood-RER-1dev:feat(FarsiNumber):create toFarsiNumberat lib-helper.js and add to component Product

--- components/Products/Product/Product.jsx
+++ components/Products/Product/Product.jsx
@@ -1,4 +1,4 @@
-import { numberFormat } from "lib/helper";
+import { numberFormat,toFarsiNumber } from "lib/helper";
 import Image from "next/image";
 import Link from "next/link";
 import { useDispatch } from "react-redux";
@@ -35,20 +35,18 @@ const Product = ({ product }) => {
         </div>
         <div className="detail-box">
           <h5>
-            <Link href={`/products/${product.slug}`}>
-              {product.name}
-            </Link>
+            <Link href={`/products/${product.slug}`}>{product.name}</Link>
           </h5>
           <p>{product.description}</p>
           <div className="options">
             <h6>
               {product.is_sale ? (
                 <>
-                  <span>{numberFormat(product.sale_price)}</span>
-                  <del className="me-1">{numberFormat(product.price)}</del>
+                  <span>{toFarsiNumber(numberFormat(product.sale_price))}</span>
+                  <del className="me-1">{toFarsiNumber(numberFormat(product.price))}</del>
                 </>
               ) : (
-                <span>{numberFormat(product.price)}</span>
+                <span>{toFarsiNumber(numberFormat(product.price))}</span>
               )}
               <span>تومان</span>
             </h6>
